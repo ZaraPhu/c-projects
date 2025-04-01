0d6bbcfa49443b5eed9ec7e9bf94b59bf241000e
Zara <zaraseshadri@Zaras-MacBook-Air.local>
2025-04-01 20:15:14 -0300

    changed create_head function to dangling_node to have similarity with next_node and prev_node. changed name of insert_node to replace_node for better clarity

--- linked-lists/singly/singly-linked.c
+++ linked-lists/singly/singly-linked.c
@@ -7,12 +7,11 @@ Creation Date: March 29, 2025.
 */
 
 /*** Dependencies ***/
-#include <stdint.h>
 #include<stdlib.h>
-#include "singly-linked.h"
+#include"singly-linked.h"
 
-/*** Functions ****/
-Node* create_head(int64_t data) {
+/*** Node Function Implementations ***/
+Node* dangling_node(int64_t data) {
     Node* new = (Node *) malloc(sizeof(Node));
     new->data = data;
     return new;
@@ -22,6 +21,13 @@ Node* next_node(int64_t data, Node* prev) {
     Node* new = (Node *) malloc(sizeof(Node));
     new->data = data;
     prev->next = new;
+    return new;
+}
+
+Node* prev_node(int64_t data, Node* next) {
+    Node* new = (Node *) malloc(sizeof(Node));
+    new->data = data;
+    new->next = prev;
     return new;
 }
 
@@ -39,8 +45,9 @@ bool search_forward(Node* head, int64_t data) {
     return false;
 }
 
+/*** SinglyLinkedList Function Implementations */
 SinglyLinkedList* create_singly_linked_list(int64_t data) {
-    Node* head = (Node *) create_head(data);
+    Node* head = (Node *) dangling_node(data);
     SinglyLinkedList* list_ptr = (SinglyLinkedList *) malloc(sizeof(SinglyLinkedList));
     list_ptr->head = head;
     return list_ptr;
@@ -58,7 +65,7 @@ void append_node(SinglyLinkedList* list_ptr, int64_t data) {
     next_node(data, node);
 }
 
-bool insert_node(SinglyLinkedList* list_ptr, int64_t data, int64_t index) {
+bool replace_node(SinglyLinkedList* list_ptr, int64_t data, int64_t index) {
     int64_t counter = 0;
     Node* node = list_ptr->head;
     while(node->next != NULL) {
--- linked-lists/singly/singly-linked.h
+++ linked-lists/singly/singly-linked.h
@@ -11,6 +11,10 @@ Creation Date: March 30, 2025.
 #include<stdint.h>
 #include<stdbool.h>
 
+
+/*** Struct Definitions ***/
+
+
 /**
  * @brief A structure representing a node in a singly linked list.
  * Contains the data for the Node, and a pointer to the next Node.
@@ -30,12 +34,16 @@ typedef struct {
     Node* head;
 } SinglyLinkedList;
 
+
+/*** Node Functions ***/
+
+
 /**
  * @brief Creates a new node with data attached but with the next Node
  *        pointer set to NULL.
  * @param data The data attached to the new node.
  */
-Node* create_head(int64_t data);
+Node* dangling_node(int64_t data);
 
 /**
  * @brief Creates a new node with data attached and initializes the given
@@ -46,26 +54,38 @@ Node* create_head(int64_t data);
 Node* next_node(int64_t data, Node* prev);
 
 /**
+ * @brief Creates a new node with data attached and initializes the
+ *        the new node's pointer to the given node.
+ * @param data The data attached to the new node.
+ */
+Node* prev_node(int64_t data, Node* next);
+
+/**
  * @brief Searches a chain of nodes, starting with the given head node
  *        and returns true if the provided data is equal to the data
- *        in one of the nodes in the link, returns false otherwise,
+ *        in one of the nodes in the link, returns false otherwise.
  */
 bool search_forward(Node* head, int64_t data);
+
+
+/*** SinglyLinkedList Functions ***/
+
 
 /**
  * @brief Creates a pointer to a new SinglyLinkedList with a head node
  *        created using the data passed as the parameter.
- * @param data the data attached to the head node of the new SinglyLinkedList
+ * @param data The data attached to the head node of the new SinglyLinkedList
  */
 SinglyLinkedList* create_singly_linked_list(int64_t data);
 
 /**
  * @brief Searches the whole SinglyLinkedList to see if it contains the data.
  *        Returns true if the data is found, returns false otherwise.
- * @param list_ptr a pointer to the SinglyLinkedList to be searched
- * @param data the data to search for
+ * @param list_ptr A pointer to the SinglyLinkedList to be searched
+ * @param data The data to search for
 */
 bool contains(SinglyLinkedList* list_ptr, int64_t data);
+
 
 /**
  * @brief Appends a node to the end of a SinglyLinkedList with the data
@@ -75,6 +95,14 @@ bool contains(SinglyLinkedList* list_ptr, int64_t data);
  */
 void append_node(SinglyLinkedList* list_ptr, int64_t data);
 
-bool insert_node(SinglyLinkedList* list_ptr, int64_t data, int64_t index);
+/**
+ * @brief Replaces the data for the node at the specified index of the 
+ *        SinglyLinkedList. Returns true if successfully replaced the data
+ *        and returns false if it couldn't reach the index.
+ * @param list_ptr a pointer to the SinglyLinkedList to be inserted into
+ * @param data the attached to the new node to be inserted
+ * @param index the index at which to insert the new node
+ */
+bool replace_node(SinglyLinkedList* list_ptr, int64_t data, int64_t index);
 
 #endif
