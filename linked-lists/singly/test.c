9ff143abaa218d9e5d5d78ec8c931d9498b616fd
Zara <zaraseshadri@Zaras-MacBook-Air.local>
2025-04-04 01:30:53 -0300

    Wrote some simple tests

--- linked-lists/singly/test.c
+++ linked-lists/singly/test.c
@@ -4,5 +4,9 @@
 
 int main() {
     SinglyLinkedList* listOfNumbers = (SinglyLinkedList *) malloc(sizeof(SinglyLinkedList));
-    
+    printf("%d\n", is_empty(listOfNumbers));
+    append_node(listOfNumbers, 12);
+    if (get(listOfNumbers, 0) != NULL) {
+        printf("%lld\n", *get(listOfNumbers, 0));
+    }
 }
